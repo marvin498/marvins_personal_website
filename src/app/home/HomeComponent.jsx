9e70990e318bbb77564d9dccd26690b75d2b7575
Marvin Moise <mmoise1@students.kennesaw.edu>
2018-10-12 18:05:56 -0400

    added hotjar for tagging

--- src/app/home/HomeComponent.jsx
+++ src/app/home/HomeComponent.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Segment} from "semantic-ui-react";
+import {hotjar} from 'react-hotjar';
+import {HOTJAR_ID, HOTJAR_SV} from './../../constants/hotJar.js'
+
+hotjar.initialize(HOTJAR_ID, HOTJAR_SV);
 
 const HomeComponent = (content) => <Segment padded vertical attached>{content.item}</Segment>
 
